9202fd177f62402f3da11d5d92288e5c48f840f2
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-09-20 20:37:57 -0400

    Everything working except Resume - Still needs CSS

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,10 +1,21 @@
 import React from "react";
+import pdf from "../Resume_Phase-2_Project.pdf"
 
 function Resume() {
+    function handleClick(e) {
+        console.log(e.target.value)
+        return (
+            <div>
+                <img src={pdf} />
+            </div>
+        )
+        
+    }
+
     return(
         <div className="hero container">
-            <h1>My Resume Goes Here</h1>
-            <a href="#" className="btn">Resume</a>
+            <h1>Click here to view my resume!</h1>
+            <button onClick={handleClick} className="btn">Resume</button>
         </div>
     )
 }
