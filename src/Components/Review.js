fbb5100918606da29b7cc89360720f75f029c16b
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-09-20 21:19:57 -0400

    Working on New Form and Cards to look better

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
 
 function Review({ review, onClickBtn }) {
   const { name, comments, likes, image, id } = review;
@@ -16,23 +20,37 @@ function Review({ review, onClickBtn }) {
         onClickBtn(updatedLike);
       });
   }
-
   return (
-    <div className="container">
-      <div>
-        <h4>If you have ever worked with me please feel free to leave a review!</h4>
-      </div>
-      <div>
-        <img src={image} alt="Profile Picture" />
-        <h2>Name: {name}</h2>
-        <p>Comment: {comments}</p>
-        <div>
-          <p>Total Likes: {likes}</p>
-          <button onClick={handleClick}>Like!</button>
-        </div>
-      </div>
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          {comments}
+        </Card.Text>
+        <Card.Text>Likes: {likes}</Card.Text>
+        <Button onClick={handleClick} variant="primary">like!</Button>
+      </Card.Body>
+    </Card>
   );
+
 }
 
 export default Review;
+
+// return (
+  //   <div className="container">
+  //     <div>
+  //       <h4>If you have ever worked with me please feel free to leave a review!</h4>
+  //     </div>
+  //     <div>
+  //       <img src={image} alt="Profile Picture" />
+  //       <h2>Name: {name}</h2>
+  //       <p>Comment: {comments}</p>
+  //       <div>
+  //         <p>Total Likes: {likes}</p>
+  //         <button onClick={handleClick}>Like!</button>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
--- src/Components/ReviewForm.js
+++ src/Components/ReviewForm.js
@@ -1,81 +1,123 @@
 import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+const initialForm = {
+  name: "",
+  image: "",
+  comments: "",
+};
 
 function ReviewForm({ handleAddNewReview }) {
-  const [formData, setFormData] = useState({
-    name: "",
-    image: "",
-    comments: "",
-  });
+  const [formData, setFormData] = useState(initialForm);
 
   function handleChange(e) {
-    console.log(e.target.value)
-    setFormData({...formData,
-    [e.target.name]: e.target.value})
+    console.log(e.target.value);
+    setFormData({ ...formData, 
+      [e.target.name]: e.target.value });
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
 
     const newReview = {
-        ...formData,
-        likes: 0,
-      }
-  
-      fetch("http://localhost:4000/Reviews", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newReview),
-      })
-        .then((resp) => resp.json())
-        .then((review) => handleAddNewReview(review));
-        setFormData(formData)
+      ...formData,
+      likes: 0,
+    };
+
+    fetch("http://localhost:4000/Reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newReview),
+    })
+      .then((resp) => resp.json())
+      .then((review) => handleAddNewReview(review));
+    setFormData(initialForm);
   }
 
- 
-
-
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <h3>Leave a review of my work!</h3>
-        <input
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Name:</Form.Label>
+        <Form.Control
+          type="text"
           value={formData.name}
-          type="text"
           name="name"
-          placeholder="Enter your name..."
-          className="input-text"
+          placeholder="Enter First and Last Name"
           onChange={handleChange}
         />
-        <br />
-        <input
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Profile Picture</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Enter a URL..."
           value={formData.image}
-          type="text"
           name="image"
-          placeholder="Enter a profile image URL..."
-          className="input-text"
           onChange={handleChange}
         />
-        <br />
-        <input
-          value={formData.comments}
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Comments:</Form.Label>
+        <Form.Control
           type="text"
+          value={formData.comments}
           name="comments"
-          placeholder="Enter your comments"
-          className="input-text"
+          placeholder="Leave your review here"
           onChange={handleChange}
         />
-        <br />
-        <input
-          type="submit"
-          name="submit"
-          value="Create New Review"
-          className="submit"
-        />
-      </form>
-    </div>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit Review
+      </Button>
+    </Form>
   );
+
+  // return (
+  //   <div className="container">
+  //     <form onSubmit={handleSubmit}>
+  //       <h3>Leave a review of my work!</h3>
+  //       <input
+  //         value={formData.name}
+  //         type="text"
+  //         name="name"
+  //         placeholder="Enter your name..."
+  //         className="input-text"
+  //         onChange={handleChange}
+  //       />
+  //       <br />
+  //       <input
+  //         value={formData.image}
+  //         type="text"
+  //         name="image"
+  //         placeholder="Enter a profile image URL..."
+  //         className="input-text"
+  //         onChange={handleChange}
+  //       />
+  //       <br />
+  //       <input
+  //         value={formData.comments}
+  //         type="text"
+  //         name="comments"
+  //         placeholder="Enter your comments"
+  //         className="input-text"
+  //         onChange={handleChange}
+  //       />
+  //       <br />
+  //       <input
+  //         type="submit"
+  //         name="submit"
+  //         value="Create New Review"
+  //         className="submit"
+  //       />
+  //     </form>
+  //   </div>
+  // );
 }
 
 export default ReviewForm;
